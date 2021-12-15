2c8111d3ed1bd8c3537ba2803a0c1fc314e9f674
lucaLuz <luiza24021@gmail.com>
2021-12-15 18:00:30 -0300

    1 commit

---
+++ peca.js
@@ -0,0 +1,20 @@
+var listaDePeca=("filtro de ar","Motor","Armortecedor")
+    if (listaDePeca < 10){
+        console.log("E possivel cadastrar mais peças!")
+    }else{
+        console.log ("Não tem mais espaço na lista.")
+    }
+    
+    let peso = 50;
+    if (peso<100){
+        console.log("A peça deve pesar no minimo 100g")
+    }else{
+        console.log("A peça possui peso adequado")
+    }
+    
+    let nomePeca ="Disco de freio"
+    if (nomePeca.length > 3){ 
+        console.log("Nome de peça esta adequado pra o cadastro!")
+    }else{
+        console.log("O nome deve ter mais de 3 caracteres,digite um nome adequado")
+    }
